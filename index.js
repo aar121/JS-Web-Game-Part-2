517dfb75e9928ce4fb72abe66f351e5f10082b5d
aar121 <arifrahim12345@gmail.com>
2023-05-14 13:23:30 -0400

    part 2 web game

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ function newImage(url, left, bottom){
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
 }
-
+let inventory = document.createElement('div');
 function newInventory(){
-    let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -39,3 +38,13 @@ newImage('assets/well.png', 500, 575)
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+
+function newItem(url, left, bottom){
+    let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url 
+        inventory.append(inventoryItem)
+    })
+}
